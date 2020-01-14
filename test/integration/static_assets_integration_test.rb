324aab123c27f3d3d0f37e0a9502107b7322111a
Johannes Koch <jkoch@systemli.org>
2020-01-14 15:28:14 +0100

    remove deprecation warnings with Minitest 6

--- test/integration/static_assets_integration_test.rb
+++ test/integration/static_assets_integration_test.rb
@@ -7,21 +7,21 @@ describe "angular-rails-templates.js integration" do
 
   it "serves angular-rails-templates.js on the pipeline" do
     visit '/assets/angular-rails-templates.js'
-    page.source.must_include %Q{angular.module("#{config.module_name}", [])}
+    _(page.source).must_include %Q{angular.module("#{config.module_name}", [])}
   end
 
   it "includes a comment with the gem name and version" do
     visit '/assets/angular-rails-templates.js'
-    page.source.must_include %Q{// Angular Rails Templates #{AngularRailsTemplates::VERSION}}
+    _(page.source).must_include %Q{// Angular Rails Templates #{AngularRailsTemplates::VERSION}}
   end
 
   it "includes a comment describing the ignore_prefix" do
     visit '/assets/angular-rails-templates.js'
-    page.source.must_include %Q{// angular_templates.ignore_prefix: #{config.ignore_prefix}}
+    _(page.source).must_include %Q{// angular_templates.ignore_prefix: #{config.ignore_prefix}}
   end
 
   it "includes a comment describing the availible markups" do
     visit '/assets/angular-rails-templates.js'
-    page.source.must_include %Q{// angular_templates.markups: #{config.markups}}
+    _(page.source).must_include %Q{// angular_templates.markups: #{config.markups}}
   end
 end
--- test/integration/user_assets_integration_test.rb
+++ test/integration/user_assets_integration_test.rb
@@ -10,19 +10,19 @@ describe "user assets integration" do
     before { visit '/assets/plain.js' }
 
     it "has a comment with the gem name" do
-      page.source.must_match   %r{// Angular Rails Template}
+      _(page.source).must_match   %r{// Angular Rails Template}
     end
 
     it "has a comment with the source file" do
-      page.source.must_include %Q{// source:}
+      _(page.source).must_include %Q{// source:}
     end
 
     it "opens an existing angular module" do
-      page.source.must_include %Q{angular.module("#{config.module_name}")}
+      _(page.source).must_include %Q{angular.module("#{config.module_name}")}
     end
 
     it "puts something in the templateCache" do
-      page.source.must_include %Q{$templateCache.put}
+      _(page.source).must_include %Q{$templateCache.put}
     end
   end
 
@@ -30,40 +30,40 @@ describe "user assets integration" do
 
     it "compiles erb (erb_template.html.erb)" do
       visit '/assets/erb_template.js'
-      page.source.must_include %Q{// source: app/assets/javascripts/erb_template.html.erb}
-      page.source.must_include %Q{$templateCache.put("erb_template.html"}
-      page.source.must_include %q{'<div class="hello-world">42</div>'}
+      _(page.source).must_include %Q{// source: app/assets/javascripts/erb_template.html.erb}
+      _(page.source).must_include %Q{$templateCache.put("erb_template.html"}
+      _(page.source).must_include %q{'<div class="hello-world">42</div>'}
     end
 
     # assuming the user loads haml
     it "compiles haml (haml_template.html.haml)" do
       visit '/assets/haml_template.js'
-      page.source.must_include %Q{// source: app/assets/javascripts/haml_template.html.haml}
-      page.source.must_include %Q{$templateCache.put("haml_template.html"}
-      page.source.must_include %q{"<h1>html-haml</h1>"}
+      _(page.source).must_include %Q{// source: app/assets/javascripts/haml_template.html.haml}
+      _(page.source).must_include %Q{$templateCache.put("haml_template.html"}
+      _(page.source).must_include %q{"<h1>html-haml</h1>"}
     end
 
     # assuming the user loads an approperate md library
     it "compiles markdown (markdown.html.md)" do
       visit '/assets/markdown.js'
-      page.source.must_include %Q{// source: app/assets/javascripts/markdown.html.md}
-      page.source.must_include %Q{$templateCache.put("markdown.html"}
-      page.source.must_match   %r{<h3.+>Markdown!</h3>}
+      _(page.source).must_include %Q{// source: app/assets/javascripts/markdown.html.md}
+      _(page.source).must_include %Q{$templateCache.put("markdown.html"}
+      _(page.source).must_match   %r{<h3.+>Markdown!</h3>}
     end
 
     it "compiles plain html (plain.html)" do
       visit '/assets/plain.js'
-      page.source.must_include %Q{// source: app/assets/javascripts/plain.html}
-      page.source.must_include %Q{$templateCache.put("plain.html"}
-      page.source.must_include %q{'<div class="hello-world">plain text</div>'}
+      _(page.source).must_include %Q{// source: app/assets/javascripts/plain.html}
+      _(page.source).must_include %Q{$templateCache.put("plain.html"}
+      _(page.source).must_include %q{'<div class="hello-world">plain text</div>'}
     end
 
     # assuming the user loads slim
     it "compiles slim (slim_template.html.slim)" do
       visit '/assets/slim_template.js'
-      page.source.must_include %Q{// source: app/assets/javascripts/slim_template.html.slim}
-      page.source.must_include %Q{$templateCache.put("slim_template.html"}
-      page.source.must_include %q{"<h1>slim template</h1>"}
+      _(page.source).must_include %Q{// source: app/assets/javascripts/slim_template.html.slim}
+      _(page.source).must_include %Q{$templateCache.put("slim_template.html"}
+      _(page.source).must_include %q{"<h1>slim template</h1>"}
     end
   end
 
@@ -76,9 +76,9 @@ describe "user assets integration" do
 
     it "compiles haml (slim_template.html.slim)" do
       visit '/assets/slim_template.js'
-      page.source.must_include %Q{// source: app/assets/javascripts/slim_template.html}
-      page.source.must_include %Q{$templateCache.put("slim_template.html"}
-      page.source.must_include %q{"<h1>slim template</h1>"}
+      _(page.source).must_include %Q{// source: app/assets/javascripts/slim_template.html}
+      _(page.source).must_include %Q{$templateCache.put("slim_template.html"}
+      _(page.source).must_include %q{"<h1>slim template</h1>"}
     end
   end
 
@@ -86,23 +86,23 @@ describe "user assets integration" do
 
     it "compiles html in a subfolder (template.html)" do
       visit '/assets/subfolder/template.js'
-      page.source.must_include %Q{// source: app/assets/javascripts/subfolder/template.html}
-      page.source.must_include %Q{$templateCache.put("subfolder/template.html"}
-      page.source.must_include %q{'<div class="hello-world">Subfolder</div>'}
+      _(page.source).must_include %Q{// source: app/assets/javascripts/subfolder/template.html}
+      _(page.source).must_include %Q{$templateCache.put("subfolder/template.html"}
+      _(page.source).must_include %q{'<div class="hello-world">Subfolder</div>'}
     end
 
     it "compiles slim in a subfolder (slim_template.html.slim)" do
       visit '/assets/subfolder/slim_template.js'
-      page.source.must_include %Q{// source: app/assets/javascripts/subfolder/slim_template.html.slim}
-      page.source.must_include %Q{$templateCache.put("subfolder/slim_template.html"}
-      page.source.must_include %q{'<div class="hello-world">Subfolder-SLIM</div>'}
+      _(page.source).must_include %Q{// source: app/assets/javascripts/subfolder/slim_template.html.slim}
+      _(page.source).must_include %Q{$templateCache.put("subfolder/slim_template.html"}
+      _(page.source).must_include %q{'<div class="hello-world">Subfolder-SLIM</div>'}
     end
 
     it "compiles haml in a subfolder (haml_template.html.haml)" do
       visit '/assets/subfolder/haml_template.js'
-      page.source.must_include %Q{// source: app/assets/javascripts/subfolder/haml_template.html.haml}
-      page.source.must_include %Q{$templateCache.put("subfolder/haml_template.html"}
-      page.source.must_include %q{"<div class='hello-world'>Subfolder-HAML</div>"}
+      _(page.source).must_include %Q{// source: app/assets/javascripts/subfolder/haml_template.html.haml}
+      _(page.source).must_include %Q{$templateCache.put("subfolder/haml_template.html"}
+      _(page.source).must_include %q{"<div class='hello-world'>Subfolder-HAML</div>"}
     end
   end
 
@@ -110,9 +110,9 @@ describe "user assets integration" do
 
     it "compiles html in a subfolder (template.html)" do
       visit '/assets/subfolder2/template.js'
-      page.source.must_include %Q{// source: app/assets/javascripts/subfolder2/template.html}
-      page.source.must_include %Q{$templateCache.put("subfolder2/template.html"}
-      page.source.must_include %q{'<div class="hello-world">Subfolder2</div>'}
+      _(page.source).must_include %Q{// source: app/assets/javascripts/subfolder2/template.html}
+      _(page.source).must_include %Q{$templateCache.put("subfolder2/template.html"}
+      _(page.source).must_include %q{'<div class="hello-world">Subfolder2</div>'}
     end
   end
 
@@ -121,23 +121,23 @@ describe "user assets integration" do
 
     it "compiles html (test.html)" do
       visit '/assets/test.js'
-      page.source.must_include %Q{// source: app/assets/templates/test.html}
-      page.source.must_include %Q{$templateCache.put("test.html"}
-      page.source.must_include %q{"outside-javascript"}
+      _(page.source).must_include %Q{// source: app/assets/templates/test.html}
+      _(page.source).must_include %Q{$templateCache.put("test.html"}
+      _(page.source).must_include %q{"outside-javascript"}
     end
 
     it "compiles str (sub/sub.html.str)" do
       visit '/assets/sub/sub.js'
-      page.source.must_include %Q{// source: app/assets/templates/sub/sub.html.str}
-      page.source.must_include %Q{$templateCache.put("sub/sub.html"}
-      page.source.must_include %q{"outside-javascript 42"}
+      _(page.source).must_include %Q{// source: app/assets/templates/sub/sub.html.str}
+      _(page.source).must_include %Q{$templateCache.put("sub/sub.html"}
+      _(page.source).must_include %q{"outside-javascript 42"}
     end
 
     it "compiles haml (sub2.html.haml)" do
       visit '/assets/sub/sub2.js'
-      page.source.must_include %Q{// source: app/assets/templates/sub/sub2.html.haml}
-      page.source.must_include %Q{$templateCache.put("sub/sub2.html"}
-      page.source.must_include %q{"<p>P.S. I love you</p>"}
+      _(page.source).must_include %Q{// source: app/assets/templates/sub/sub2.html.haml}
+      _(page.source).must_include %Q{$templateCache.put("sub/sub2.html"}
+      _(page.source).must_include %q{"<p>P.S. I love you</p>"}
     end
   end
 
